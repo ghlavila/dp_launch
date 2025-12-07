4db65a62970025de02c7b7152acec9c817c95263
Luis Avila <lavila@techsafari.com>
2025-12-07 06:48:39 -0500

    setup for local launch

--- dev_box.sh
+++ dev_box.sh
@@ -1,1 +1,2 @@
-dp_utils-local launch-pipeline --config lavila.yaml --client-id lavila --table dev --instance-type m6id.2xlarge --keep-running
+dp_utils-local launch-pipeline --config dp_dev.yaml --client-id lavila --table dev --instance-type m6id.2xlarge --keep-running
+#dp_utils-local launch-pipeline --config /data/git/data_processing/command_runner/gh-data-proc.yaml --client-id lavila --table dev --instance-type m6id.2xlarge
